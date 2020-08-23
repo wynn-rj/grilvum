15814ab4d82147035e9e20b82e91edc9de6aad12
Ryan Havanas <rhavanas@gmail.com>
2020-08-23 17:09:04 -0400

    additional error handling, and minor QOL changes.

--- bag_of_holding.py
+++ bag_of_holding.py
@@ -45,7 +45,8 @@ class BagOfHolding(commands.Cog):
             cur.execute("SELECT ID FROM bag_of_holding WHERE Guild_ID = ? AND Name = ?",(guild.id, name))
             result = cur.fetchall()
             if len(result) > 0:
-                cur.execute("DELETE FROM bag_of_holding WHERE Name = ? AND Guild_ID = ?", (name, guild.id))   
+                cur.execute("DELETE FROM bag_of_holding_items WHERE Bag_ID = (SELECT ID FROM bag_of_holding WHERE Name = ? and Guild_ID = ?)", (name, guild.id))
+                cur.execute("DELETE FROM bag_of_holding WHERE Name = ? AND Guild_ID = ?", (name, guild.id)) 
                 conn.commit()
                 return 1
             else:
@@ -77,22 +78,26 @@ class BagOfHolding(commands.Cog):
     def remove_from_bag(self, guild, name, item, quantity):
         conn = sqlite3.connect(self.config.data.database)
         try:
+            removed = 0
             cur = conn.cursor()        
             cur.execute("SELECT Quantity FROM bag_of_holding_items WHERE Item = ? AND Bag_ID = (SELECT ID FROM bag_of_holding WHERE Name = ? and Guild_ID = ?)", (item, name, guild.id))
             result = cur.fetchone()
             if result is not None:
-                if int(result[0]) - int(quantity) < 1:
+                dif = int(result[0]) - int(quantity)
+                if dif < 1:
                     cur.execute("DELETE FROM bag_of_holding_items WHERE Item = ? AND Bag_ID = (SELECT ID FROM bag_of_holding WHERE Name = ? and Guild_ID = ?)", (item, name, guild.id))
+                    removed = result[0]
                 else:
-                    cur.execute("UPDATE bag_of_holding_items SET Quantity = ? WHERE Item = ? AND Bag_ID = (SELECT ID FROM bag_of_holding WHERE Name = ? and Guild_ID = ?)", (int(result[0]) - int(quantity), item, name, guild.id))
+                    cur.execute("UPDATE bag_of_holding_items SET Quantity = ? WHERE Item = ? AND Bag_ID = (SELECT ID FROM bag_of_holding WHERE Name = ? and Guild_ID = ?)", (dif, item, name, guild.id))
+                    removed = quantity
             else:
-                return 0
+                removed = 0
             conn.commit()
-            return 1
+            return removed
         finally:
             conn.close()
 
-    @commands.group(pass_context=True, help="Manage bag of holding")
+    @commands.group(pass_context=True, help="Manage bag of holding.")
     async def boh(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.send('Invalid subcommand')
@@ -123,19 +128,24 @@ class BagOfHolding(commands.Cog):
         result = self.add_to_bag(ctx.guild, name, item, quantity)
         await ctx.send(f'{result}')
         if result == 1:
-            await ctx.send(f'{item}({quantity}) successfully added to the Bag of Holding')
+            if quantity > 1:
+                await ctx.send(f'{item}({quantity}) successfully added to the Bag of Holding.')
+            else:
+                await ctx.send(f'{item} successfully added to the Bag of Holding.')
         else:
-            await ctx.send(f'Error adding {item}({quantity}) to {name}')
+            await ctx.send(f'Error adding {item}({quantity}) to {name}.')
 
 
     @boh.command()
     async def dump(self, ctx, name):
         party_bag = self.get_party_bag(ctx.guild, name)
         if party_bag is None:
-            await ctx.send(f'Failed to get {name}')
+            await ctx.send(f'Failed to get {name}.')
             return
-
-        print_string = 'The Bag of Holding Contains {}:'.format(len(party_bag))
+        if len(party_bag) == 0:
+            await ctx.send(f'{name} is empty.')
+            return
+        print_string = 'The Bag of Holding Contains:'
         for item in party_bag:
             print_string += f'\n{item[0]}'
             if item[1] > 1:
@@ -147,10 +157,13 @@ class BagOfHolding(commands.Cog):
     @boh.command()
     async def remove(self, ctx, name, item: str, quantity = 1):
         result = self.remove_from_bag(ctx.guild, name, item, quantity)
-        if result == 1:
-            await ctx.send(f'{item}({quantity}) removed from {name}')
+        if result > 0:
+            if quantity > 1:
+                await ctx.send(f'{item}({result}) removed from {name}.')
+            else:
+                await ctx.send(f'{item} removed from {name}.')
         else:
-            await ctx.send(f'{item} not found')
+           await ctx.send(f'{item} not found.')
 
 
 
